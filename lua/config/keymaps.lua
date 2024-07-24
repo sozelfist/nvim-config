06198deb7ba05e68fd87ef76a0ea1378316d93ef
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-07-24 20:30:51 +0700

    feat: add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,29 @@ keymap.set("n", "dw", 'vb"_d', opts)
 
 -- Switch between nvim windows
 keymap.set("n", "<tab>", "<C-w>w", opts)
+
+-- Increment/decrement
+keymap.set("n", "-", "<C-x>")
+keymap.set("n", "+", "<C-a>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Split window
+keymap.set("n", "ss", "<C-w>s", opts)
+keymap.set("n", "sv", "<C-w>v", opts)
+
+-- Move windows
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Quit window
+keymap.set("n", "sq", "<C-w>q")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
