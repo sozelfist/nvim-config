e547a72044a188328d196bb44729abb115401ba0
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-10-27 18:10:07 +0700

    fix: fix reversed lualine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,10 @@ return {
   -- add gruvbox
   {
     "ellisonleao/gruvbox.nvim",
-    -- opts = {
-    --   transparent_mode = true,
-    -- },
+    opts = {
+      -- transparent_mode = true,
+      inverse = false,
+    },
   },
 
   -- Configure LazyVim to load gruvbox
