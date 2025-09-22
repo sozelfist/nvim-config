29dea478745636af9860c9613f2cd7985f162736
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2025-09-22 20:33:12 +0700

    fix: remove nvim-treesitter.query_predicates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,7 +11,7 @@ return {
     -- Luckily, the only things that those plugins need are the custom queries, which we make available
     -- during startup.
     require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
+    -- require("nvim-treesitter.query_predicates")
   end,
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   keys = {
